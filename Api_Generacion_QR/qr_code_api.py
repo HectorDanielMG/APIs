8028f74def8a614b1095e1dd4f8933ff26cb05f2
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-10-05 14:16:55 -0600

    Generacion de QR redirigiendo a mi pagina de GitHub

--- Api_Generacion_QR/qr_code_api.py
+++ Api_Generacion_QR/qr_code_api.py
@@ -1,15 +1,13 @@
-from flask import Flask, request, send_file
+from flask import Flask, render_template, make_response
 import qrcode
 from io import BytesIO
 
 app = Flask(__name__)
 
-@app.route('/generate_qr', methods=['GET'])
-def generate_qr():
-    data = request.args.get('data')
-
-    if not data:
-        return {'error': 'No se proporcionó ningún dato'}, 400
+@app.route('/')
+def index():
+    # URL a convertir en QR
+    url = "https://github.com/HectorDanielMG"
 
     # Generar el código QR
     qr = qrcode.QRCode(
@@ -18,18 +16,21 @@ def generate_qr():
         box_size=10,
         border=4,
     )
-    qr.add_data(data)
+    qr.add_data(url)
     qr.make(fit=True)
 
     img = qr.make_image(fill='black', back_color='white')
 
-    # Guardar la imagen en memoria
+    # Guardar el código QR en un objeto BytesIO
     img_io = BytesIO()
     img.save(img_io, 'PNG')
     img_io.seek(0)
 
-    # Enviar la imagen al cliente
-    return send_file(img_io, mimetype='image/png')
+    # Devolver el código QR como una imagen en la respuesta
+    response = make_response(img_io.read())
+    response.headers.set('Content-Type', 'image/png')
+    response.headers.set('Content-Disposition', 'inline', filename='qr_code.png')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
