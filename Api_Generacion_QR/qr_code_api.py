e3bdbf84de85b5e2653f6d56acb90e9448f20d09
HectorDanielMG <hectormarmolejogarcia@gmail.com>
2024-10-23 23:41:52 -0600

    Descarga directa del QR

--- Api_Generacion_QR/qr_code_api.py
+++ Api_Generacion_QR/qr_code_api.py
@@ -8,14 +8,16 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        # Obtener la URL del formulario
+        # Obtener los datos del formulario
         url = request.form.get('url')
+        fill_color = request.form.get('fill_color') or 'black'
+        back_color = request.form.get('back_color') or 'white'
         
         # Validar la URL
         if not validators.url(url):
             return render_template('index.html', error="URL inválida. Por favor, introduce una URL válida.")
 
-        # Generar el código QR
+        # Generar el código QR con colores personalizados
         qr = qrcode.QRCode(
             version=1,
             error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -25,17 +27,17 @@ def index():
         qr.add_data(url)
         qr.make(fit=True)
 
-        img = qr.make_image(fill='black', back_color='white')
+        img = qr.make_image(fill=fill_color, back_color=back_color)
 
         # Guardar el código QR en un objeto BytesIO
         img_io = BytesIO()
         img.save(img_io, 'PNG')
         img_io.seek(0)
 
-        # Devolver el código QR como una imagen en la respuesta
+        # Devolver el código QR como una imagen en la respuesta para visualizar y descargar
         response = make_response(img_io.read())
         response.headers.set('Content-Type', 'image/png')
-        response.headers.set('Content-Disposition', 'inline', filename='qr_code.png')
+        response.headers.set('Content-Disposition', 'attachment', filename='qr_code.png')
         return response
     
     # Mostrar la página de inicio con el formulario
